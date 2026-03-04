c17078e2225d6a09fd9224b5ab5f1225443c109d
Shreyansh <shreyanshloop07@gmail.com>
2026-03-04 15:26:19 +0530

    feat: Add MongoDB connection and Mongoose setup

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+await connectDB();
+
 const app = express();
 
 app.use(cors({
